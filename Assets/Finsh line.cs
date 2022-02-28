bd57badfad437c7ece8511cc19426f7bf312563d
RobertWooley <wooley278@morrisville.edu>
2022-02-28 15:11:17 -0500

    Initial commit

---
+++ Assets/Finsh line.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("your Finush");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player contoler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontoler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { {SerializeField
+        GetComponent <reichit body >
+    }
+
+    // Update is called once per frame
+    void Update()
+   { if(Input.GetKeyUp.RightArrow)
+
+   
+    { 
+        rb2d
+    } if(Input.keyCode.LeftAreow)
+    }
